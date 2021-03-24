7c60f53f0b91e275b8c416c67212497f88958481
Chadwell-Clark <wcclark23@comcast.net>
2021-03-23 21:58:31 -0500

    Refactored to use Post entry for entry and edit

--- src/scripts/feed/PostEntry.js
+++ src/scripts/feed/PostEntry.js
@@ -1,5 +1,5 @@
 //   ***  Post entry HTML input form  ready for export  
-
+//   ***  Original below  ***   //
 export const PostEntry = () => {
   return `
         <form class="newPost">
@@ -27,3 +27,52 @@ export const PostEntry = () => {
         </form>
         `;
 };
+
+
+// //   !!!  Refactor Below  !!!   //
+
+// let title, imgURL, desc, postPartA, postPartC = "";
+
+// export const PostEntry = (postObj) => {
+//     if (postObj === undefined) {
+//        title, imgURL, desc ="";
+//         postPartA = `<form class="newPost">`;
+//         postPartC = `<button id="newPost__submit">Save</button>
+//             <button id="newPost__cancel">Cancel</button>
+//         </form>`;
+//     } else {
+//         postPartA = `<div class="newPost">
+// 	        <h3>Edit This Post</h3>`;
+//         postPartC = `<input type="hidden" value="${postObj.id}" name="postId">
+// 		    <input type="hidden" value="${postObj.timestamp}" name="postTime">	
+// 		    <button id="updatePost__${postObj.id}">Update</button>
+// 		    <button id="newPost__cancel">Cancel</button>
+// 	        </div>`;
+//     title = postObj.title;
+//     imgURL = postObj.imageURL;
+//     desc = postObj.description;
+//     }
+//     console.log ("title: ", title, "IMG: ", imgURL, "description: ", desc)
+//     return `
+//     ${postPartA}
+//     <div>
+// 			<input value="${title}"
+// 				   name="postTitle"
+// 				   class="newPost__input"
+// 				   type="text"
+// 				   placeholder="Title" />
+// 		</div>
+// 		<div>
+// 			<input value="${imgURL}"
+// 				   name="postURL"
+// 				   class="newPost__input"
+// 				   type="text"
+// 				   placeholder="URL of gif" />
+// 		</div>
+
+//     	<textarea name="postDescription"
+//     	class="newPost__input newPost__description"
+//     	placeholder="Story behind your gif...">${desc}</textarea>
+//         ${postPartC}
+//     `;
+// }
